28c5f0f6c153cb5c5c843f2376ceaba218b6dec1
Andrea055 <vivanco11.ac@gmail.com>
2023-04-02 19:35:36 +0000

    arch: kona-perf regen

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+config=kona-perf_defconfig
+
+SOURCE_ROOT=~/workspace/source
+MAKE_PATH=${SOURCE_ROOT}/prebuilts/build-tools/linux-x86/bin/
+CROSS_COMPILE=/home/andreock/workspace/source/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+KERNEL_ARCH=arm64
+KERNEL_DIR=${SOURCE_ROOT}/kernel/realme_GTneo2-Q5pro-GTMasterExploratory-AndroidT-kernel-source
+KERNEL_OUT=${KERNEL_DIR}/../kernel_out
+export KERNEL_SRC=${KERNEL_OUT}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+OUT_DIR=${KERNEL_OUT}
+ARCH=${KERNEL_ARCH}
+TARGET_INCLUDES=${TARGET_KERNEL_MAKE_CFLAGS}
+TARGET_LINCLUDES=${TARGET_KERNEL_MAKE_LDFLAGS}
+
+TARGET_KERNEL_MAKE_ENV+="CC=${SOURCE_ROOT}/prebuilts/clang/host/linux-x86/bin/clang"
+
+cd ${KERNEL_DIR} && \
+${MAKE_PATH}make O=${OUT_DIR} ${TARGET_KERNEL_MAKE_ENV} HOSTLDFLAGS="${TARGET_LINCLUDES}" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j16 vendor/$config
+
+# cd ${KERNEL_DIR} && \
+# ${MAKE_PATH}make O=${OUT_DIR} ${TARGET_KERNEL_MAKE_ENV} HOSTLDFLAGS="${TARGET_LINCLUDES}" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} menuconfig
+
+cd ${OUT_DIR} && \
+${MAKE_PATH}make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} HOSTCFLAGS="${TARGET_INCLUDES}" HOSTLDFLAGS="${TARGET_LINCLUDES}" O=${OUT_DIR} ${TARGET_KERNEL_MAKE_ENV} NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip  -j16
