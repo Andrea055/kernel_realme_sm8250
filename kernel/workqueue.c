a1c36e3f0ad2bbc30321972701afc88e1d08b786
Andrea055 <vivanco11.ac@gmail.com>
2023-05-31 10:19:27 +0200

    Revert "workqueue: Remove now redundant lock acquisitions wrt. workqueue flushes"
    This reverts commit 536b917ef45e2a66323cdaa1604a63a9bc8c348b.

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -2701,6 +2701,9 @@ void flush_workqueue(struct workqueue_struct *wq)
 	if (WARN_ON(!wq_online))
 		return;
 
+	lock_map_acquire(&wq->lockdep_map);
+	lock_map_release(&wq->lockdep_map);
+
 	mutex_lock(&wq->mutex);
 
 	/*
@@ -2954,7 +2957,13 @@ static bool __flush_work(struct work_struct *work, bool from_cancel)
 	if (WARN_ON(!wq_online))
 		return false;
 
-	if (start_flush_work(work, &barr)) {
+	if (WARN_ON(!work->func))
+		return false;
+
+	lock_map_acquire(&work->lockdep_map);
+	lock_map_release(&work->lockdep_map);
+
+	if (start_flush_work(work, &barr, from_cancel)) {
 		wait_for_completion(&barr.done);
 		destroy_work_on_stack(&barr.work);
 		return true;
